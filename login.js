aea8092b096167c461382b77a9d9f7370a55876b
Irfan Sattar <23389884+irfansattar@users.noreply.github.com>
2018-11-25 20:16:38 -0800

    Implementation of Firebase authentication for login page

---
+++ login.js
@@ -0,0 +1,60 @@
+function onload()
+{
+    var user = firebase.auth().currentUser;
+
+    if (user) 
+    {
+        console.log("Logged in");
+        window.location='Andrew Canafe\\index.html';
+    }
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+
+    console.log("In onAuthStateChanged");
+
+    if (user) 
+    {
+        console.log("Logged in");
+        window.location='Andrew Canafe\\index.html';
+    }
+    else
+    {
+        console.log("Not logged in");
+        // window.location='index.html';
+    }
+});
+
+function login() {
+    // console.log('In login function');
+
+    email = document.getElementById("email").value;
+    password = document.getElementById("password").value;
+
+    if (email.length === 0 || password.length === 0) {
+        console.log("Enter username and/or password");
+        return;
+    }
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        if (errorCode === 'auth/wrong-password')
+        {
+            console.log("Incorrect password");
+            document.getElementById("incorrect-password").style.display = "block";
+        }
+        else if (errorCode === 'auth/invalid-email' || errorCode === 'auth/user-not-found')
+        {
+            console.log("Incorrect email or the user does not exist")
+            document.getElementById("incorrect-email").style.display = "block";
+        }
+
+        console.log(errorCode);
+        console.log(errorMessage);
+        console.log("End of login function");
+        // ...
+    });
+}
